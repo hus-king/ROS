7764b4f17fddb097f9bc3f95073cf80be834fb3a
hus-king <3334991525@qq.com>
2025-01-22 17:32:53 +0800

    修复条件判断中的逻辑错误，确保正确处理高度数据

--- 5_winter/winter.cpp
+++ 5_winter/winter.cpp
@@ -491,7 +491,7 @@ int linefind(float height[181]) {
     line[0].start = 0;
     int key = 0;
     for (int i = 0; i < 180; i++) {
-        if (minus[i] < 0.2 !isinf(height[i]) {
+        if (minus[i] < 0.2 && !isinf(height[i]) {
             line[key].length++;
             line[key].end = i + 1;
         } else {
